ed98e38ee4de4315d830057c75cda8dc63cd2835
praneesh-webcompanies <42199713+praneesh-webcompanies@users.noreply.github.com>
2018-08-10 14:11:15 +0400

    testing

--- wp-content/themes/WebCompanies/index.php
+++ wp-content/themes/WebCompanies/index.php
@@ -38,6 +38,7 @@ get_header();
      * Include the Post-Type-specific template for the content.
      * If you want to override this in a child theme, then include a file
      * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+     * Test
      */
     get_template_part('template-parts/content', get_post_type());
 
